ed67e6c2f2202ba2d1eac9ddd4f822483733b961
allasriharsha <43423826+allasriharsha@users.noreply.github.com>
2023-12-05 09:49:04 -0800

    Update App.js

--- webapp1/src/App.js
+++ webapp1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BlueNest Soft <code>src/App.js</code> and save to reload.Testing
+          BlueNest Soft Coming Soon ....
         </p>
       
         </a>
